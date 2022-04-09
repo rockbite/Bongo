245092e55d157dba21d20aa37632611e746c3263
Tom Wojciechowski <tomwojciechowski@asidik.com>
2022-04-09 18:37:11 +0100

    dependencies management overhaul

--- engine/src/main/java/com/rockbite/bongo/engine/systems/render/EngineDebugEndSystem.java
+++ engine/src/main/java/com/rockbite/bongo/engine/systems/render/EngineDebugEndSystem.java
@@ -2,8 +2,6 @@ package com.rockbite.bongo.engine.systems.render;
 
 import com.artemis.BaseSystem;
 import com.rockbite.bongo.engine.Bongo;
-import imgui.ImGui;
-import imgui.gl3.ImGuiImplGl3;
 
 public class EngineDebugEndSystem extends BaseSystem {
 	@Override
